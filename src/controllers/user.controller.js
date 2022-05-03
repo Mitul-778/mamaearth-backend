8444bf6f4db7fa6cf56013663695e43f2a353a3b
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-05-03 13:56:01 +0530

    user model created

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const User = require("../models/user.model");
+const router = express.Router();
+
+router.get("", async(req,res) =>{
+    try {
+        const users = await User.find().lean().exec();
+        return res.status(200).send(users);
+    } catch (error) {
+        return res.status(500).send({err : error.message})
+    }
+})
+
+router.post("", async(req,res) =>{
+    try {
+        const user = await User.create(req.body);
+        return res.status(201).send(user);
+    } catch (error) {
+        return res.status(500).send({err : error.message})
+    }
+})
+
+module.exports = router
